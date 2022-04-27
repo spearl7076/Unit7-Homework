5aa21ee8f69ca6132d8e1a5eed96afb2d58f967d
Famous Samos <spearlman2@gmail.com>
2022-04-27 19:19:29 -0400

    added the views from the sql queries...HW complete

--- Data/fraud_detection_views.sql
+++ Data/fraud_detection_views.sql
@@ -1,10 +1,21 @@
--- Isolates/groups the transactiosn of each cardholder
+DROP VIEW IF EXISTS cardholder_transactions CASCADE;
+DROP VIEW IF EXISTS cardholder25_transactions CASCADE;
+DROP VIEW IF EXISTS small_transactions CASCADE;
+DROP VIEW IF EXISTS large_transactions CASCADE;
+DROP VIEW IF EXISTS transactions7_9 CASCADE;
+DROP VIEW IF EXISTS transactionsNOT_7_9 CASCADE;
+DROP VIEW IF EXISTS fraudulent_merchants CASCADE;
+
+-- View of query that isolates/groups the transactiosn of each cardholder
+CREATE VIEW cardholder_transactions AS
 SELECT credit_card.cardholder_id, transaction.date, transaction.amount
 FROM credit_card
 LEFT JOIN transaction
 ON credit_card.card = transaction.card;
 
---Grabs the Transaction Month Number, Day Number, and Amount for Cardholder 25
+
+--View of Cardholder 25 Transactions by Month Number and Day number
+CREATE VIEW cardholder25_transactions AS
 SELECT EXTRACT(MONTH FROM transaction.date) as month,EXTRACT(DAY FROM transaction.date) as day, transaction.amount
 FROM credit_card
 LEFT JOIN transaction
@@ -13,7 +24,8 @@ WHERE transaction.date between '2018-01-01' and '2018-06-30' and
 credit_card.cardholder_id  = 25;
 
 
--- Counts the transactions that are less than $2.00 per cardoholder
+-- View of the number of transactions that are less than $2.00 per cardoholder
+CREATE VIEW small_transactions AS
 SELECT credit_card.cardholder_id, COUNT(transaction.amount) as "Small Transactions"
 FROM credit_card
 LEFT JOIN transaction
@@ -23,16 +35,17 @@ GROUP BY credit_card.cardholder_id
 ORDER BY credit_card.cardholder_id ASC;
 
 
--- Counts the transactions that are more than $500.00 per cardoholder
-SELECT credit_card.cardholder_id, COUNT(transaction.amount) as "Large Transactions"
+-- View of the number of transactions that are larger than $500.00 per cardoholder
+CREATE VIEW large_transactions AS
+SELECT credit_card.cardholder_id, COUNT(transaction.amount) as "Small Transactions"
 FROM credit_card
 LEFT JOIN transaction
 ON (credit_card.card = transaction.card)
 WHERE transaction.amount > 500
 GROUP BY credit_card.cardholder_id;
 
-
--- Lists the top 100 transactions between 7am and 9am
+-- View of top 100 transactions between 7am and 9am
+CREATE VIEW transactions7_9 AS
 SELECT credit_card.cardholder_id,transaction.date, transaction.amount
 FROM credit_card
 LEFT JOIN transaction
@@ -42,7 +55,8 @@ ORDER BY transaction.amount DESC
 LIMIT 100;
 
 
--- Lists the top 100 transactions that occure before 7am and after 9am
+-- View of top 100 transactions that occure before 7am and after 9am
+CREATE VIEW transactionsNOT_7_9 AS
 SELECT credit_card.cardholder_id,transaction.date, transaction.amount
 FROM credit_card
 LEFT JOIN transaction
@@ -52,7 +66,8 @@ ORDER BY transaction.amount DESC
 LIMIT 100;
 
 
--- Merchant types that have the most transactions smaller than $2.00
+-- View of Merchant types that have the most transactions smaller than $2.00
+CREATE VIEW fraudulent_merchants AS
 SELECT merchant_category.name, COUNT(transaction.amount) as "Small Transactions"
 FROM transaction
 JOIN merchant
@@ -62,3 +77,6 @@ JOIN merchant_category
 WHERE transaction.amount < 2.00
 GROUP BY merchant_category.name
 ORDER by COUNT(transaction.amount) DESC;
+
+
+
